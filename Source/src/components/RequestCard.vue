a9ed9e930abbe7526a9331ebc7e8abd12f0bc7d3
Lydia Winters <lydia@dev-squared.com>
2019-09-20 15:06:54 -0500

    fixed-it

--- Source/src/components/RequestCard.vue
+++ Source/src/components/RequestCard.vue
@@ -1,43 +1,4 @@
-﻿<template>
-
-
-  <div class="card">
-    <div class="ml-3">
-      <p><b>Time:</b> {{request.dateTime}}</p>
-      <p><b>Request:</b> {{request.problem}}</p>
-      <p><b>Requester:</b> {{request.name}}</p>
-      <div class="row" style="padding:10px 10px;">
-        <h3>{{request.numberOfPeople}}</h3>
-        <div style="padding:0px 30px" />
-        <a href="#" class="button" @click="joinSession()">+ Join Session</a>
-      </div>
-
-    </div>
-  </div>
-
-</template>
-
-<script>
-  import axios from 'axios';
-  export default {
-    name: 'RequestCard',
-    props: {
-      request: {
-        type: Object,
-        default: () => { }
-      },
-    },
-    methods: {
-      async joinSession() {
-        const { data } = await axios.put('/api/tutoringrequest/join?requestid=' + this.request.id);
-        this.request = data;
-      }
-    },
-  }
-
-</script>
-
-<style scoped>
+﻿<style scoped>
   .button {
     background-color: #64CC96;
     -moz-border-radius: 17px;
@@ -64,17 +25,61 @@
 
   .card {
     border: 1px #efefef solid;
-    -webkit-box-shadow: 10px 10px 10px -2px rgba(219, 219, 219, 1);
-    -moz-box-shadow: 10px 10px 10px -2px rgba(219, 219, 219, 1);
-    box-shadow: 10px 10px 10px -2px rgba(219, 219, 219, 1);
     transition-timing-function: ease-in;
     transition-property: box-shadow;
     transition-duration: .1s;
     transition-timing-function: linear;
     text-align: left;
-    padding: 15px;
+    padding: 10px;
     background-color: #F5F4F4;
     border-radius: 15px;
-    width: 340px;
+    width: 100%;
   }
 </style>
+
+
+<template>
+
+
+  <div class="card shadow-sm ">
+    <div class="m-2">
+      <p><b>Time:</b> {{request.dateTime | formatDate}}</p>
+      <p><b>Request:</b> {{request.problem}}</p>
+      <p><b>Requester:</b> {{request.name}}</p>
+      <div class="row" style="padding:10px 10px;">
+        <h3>{{request.numberOfPeople}}</h3>
+        <div style="padding:0px 30px" />
+        <a href="#" class="button" @click="joinSession()">+ Join Session</a>
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import axios from 'axios';
+  import dayjs from 'dayjs';
+  export default {
+    name: 'RequestCard',
+    props: {
+      request: {
+        type: Object,
+        default: () => { }
+      },
+    },
+    methods: {
+      async joinSession() {
+        const { data } = await axios.put('/api/tutoringrequest/join?requestid=' + this.request.id);
+        this.request = data;
+      }
+    },
+    filters: {
+      formatDate(date) {
+        const value = dayjs(date).format('MM/DD/YYYY hh:mm A')
+        return value
+      }
+    }
+  }
+
+</script>
--- Source/src/components/TutorRequests.vue
+++ Source/src/components/TutorRequests.vue
@@ -1,15 +1,22 @@
-﻿<template>
+﻿<style scoped>
+    .tutor-request-card {
+        background-color: #F5F4F4;
+
+    }
+</style>
+
+<template>
     <div v-if="loading"><img src="/images/loader.gif" alt=""></div>
-    <div v-else class="container ">
-        <div class="card">
+    <div v-else class="card">
+        <div class="border">
             <div class="tutor-request-card" style="height:50px">
-                <div class="pl-2">
+                <div class="pl-2 ">
                     <span class="h1 pl-2">{{ tutor.subject }}</span> <span class="pl-5"><b>Name:</b> {{tutor.firstName}} {{tutor.lastName}}</span> <span class="pl-5"><b>Location:</b> {{tutor.location}}</span> <span class="pl-5"><b>Time:</b>{{tutor.time}}</span>
                 </div>
             </div>
         </div>
-        <div class="row ">
-            <div v-for="request in tutoringRequests" class="col-md-4 " :key="request.id">
+        <div class="row mx-0">
+            <div v-for="request in tutoringRequests" class="col-md-4 p-4 " :key="request.id">
                 <request-card :request="request"></request-card>
             </div>
         </div>
@@ -49,17 +56,3 @@
         },
     };
 </script>
-
-
-<style scoped>
-    .tutor-request-card {
-        background-color: #F5F4F4;
-
-    }
-
-    .card {
-        text-align: left;
-        margin: 30px;
-
-    }
-</style>
