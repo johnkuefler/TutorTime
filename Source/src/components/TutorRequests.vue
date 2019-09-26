3c7ed796301d215493c91617de4214b17d01ed22
John Kuefler <john@dev-squared.com>
2019-09-26 17:40:50 -0500

    added tutor request

--- Source/src/components/TutorRequests.vue
+++ Source/src/components/TutorRequests.vue
@@ -3,25 +3,88 @@
         background-color: #F5F4F4;
 
     }
+
+      .button {
+        background-color: #64CC96;
+        -moz-border-radius: 17px;
+        -webkit-border-radius: 17px;
+        border-radius: 17px;
+        display: inline-block;
+        cursor: pointer;
+        color: #000000;
+        font-family: Arial;
+        font-size: 17px;
+        font-weight: bold;
+        padding: 10px 10px;
+        text-decoration: none;
+      }
+
+      .button:hover {
+        background-color: #C8F2DC;
+      }
+
+      .button:active {
+        position: relative;
+        top: 1px;
+      }
 </style>
 
 <template>
-    <div v-if="loading"><img src="/images/loader.gif" alt=""></div>
-    <div v-else class="card">
-        <div class="border">
-            <div class="tutor-request-card" style="height:50px">
-                <div class="pl-2 ">
-                    <span class="h1 pl-2">{{ tutor.subject }}</span> <span class="pl-5"><b>Name:</b> {{tutor.firstName}} {{tutor.lastName}}</span> <span class="pl-5"><b>Location:</b> {{tutor.location}}</span> <span class="pl-5"><b>Time:</b>{{tutor.time}}</span>
+    <div>
+        <div v-if="loading"><img src="/images/loader.gif" alt=""></div>
+        <div v-else class="card">
+            <div class="border">
+                <div class="tutor-request-card py-3">
+                    <div class="pl-2 ">
+                        <span class="h1 pl-2">{{ tutor.subject }}</span> <span class="pl-5"><b>Name:</b> {{tutor.firstName}} {{tutor.lastName}}</span> <span class="pl-5"><b>Location:</b> {{tutor.location}}</span> <span class="pl-5"><b>Time:</b>{{tutor.time}}</span>
+                        <a href="#" class="button mx-5" @click="launchCreateSessionModal()">+ Add Session</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mx-0">
+                <div v-for="request in tutoringRequests" class="col-md-4 p-4 " :key="request.id">
+                    <request-card :request="request"></request-card>
                 </div>
             </div>
         </div>
-        <div class="row mx-0">
-            <div v-for="request in tutoringRequests" class="col-md-4 p-4 " :key="request.id">
-                <request-card :request="request"></request-card>
-            </div>
-        </div>
-    </div>
 
+
+        <modal name="request-modal" height="auto" :scrollable="true">
+            <div class="px-5 py-3">
+                <div class="row">
+                    <div class="col-12">
+                        <h4>Add Request</h4>
+                    </div>
+                </div>
+                <div class="row  mt-3">
+                    <div class="col-12">
+                        <input class="form-control" placeholder="Your Name" v-model="requestForm.name">
+                    </div>
+                </div>
+                <div class="row  mt-3">
+                    <div class="col-12">
+                        <input class="form-control" placeholder="Problem" v-model="requestForm.problem">
+                    </div>
+                </div>
+                <div class="row  mt-3">
+                    <div class="col-12">
+                        <input type="datetime-local" class="form-control" placeholder="Date/Time" v-model="requestForm.dateTime">
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-2 offset-8">
+                        <button type="button" class="btn" @click="$modal.hide('request-modal')">Cancel</button>
+                    </div>
+                    <div class="col-2">
+                        <button type="button" class="button"
+                                @click="submitRequest">
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </modal>
+    </div>
 </template>
 
 <script>
@@ -32,6 +95,13 @@
         data: () => ({
             tutoringRequests: [],
             loading: false,
+            requestForm: {
+                tutorId: '',
+                name: '',
+                numberOfPeople: 1,
+                problem: '',
+                dateTime: new Date()
+            }
         }),
         props: {
             tutor: {
@@ -52,6 +122,17 @@
                 this.tutoringRequests = data;
                 this.loading = false;
             },
+            launchCreateSessionModal() {
+               this.$modal.show('request-modal');
+            },
+            async submitRequest() {
+                this.requestForm.tutorId = this.tutor.id;
+
+               this.loading = true;
+                await axios.post('/api/tutoringrequest/', this.requestForm);
+               this.$modal.hide('request-modal');
+               await this.getTutorRequests();
+            }
 
         },
     };
--- Source/src/main.js
+++ Source/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import VModal from 'vue-js-modal'
+
 Vue.config.productionTip = false
 Vue.component('tutors-list', require('./components/TutorsList.vue').default);
 Vue.component('tutor-card', require('./components/TutorCard.vue').default);
 Vue.component('tutor-requests', require('./components/TutorRequests.vue').default);
 Vue.component('request-card', require('./components/RequestCard.vue').default);
+Vue.use(VModal);
 
 
 
